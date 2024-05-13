a897f012121524138f089ae7b79dd7bd1ea621b8
Chase Sheaff <chasefsheaff@gmail.com>
2024-05-13 16:47:51 -0500

    add CKEditor and render on lesson plan page

--- app/subject/[unitId]/[lessonId]/page.tsx
+++ app/subject/[unitId]/[lessonId]/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import Surface from '../../../../components/surface/Surface';
 import TextField from '../../../../components/TextField';
 import Textarea from '../../../../components/Textarea';
+import CKeditor from '../../../../components/CKeditor';
 
 export default function Unit({
   params,
@@ -15,8 +16,15 @@ export default function Unit({
   const [title, setTitle] = useState<string | null>(null);
   const [objective, setObjective] = useState<string | null>(null);
   const [standard, setStandard] = useState<string | null>(null);
+  const [body, setBody] = useState<string | null>(null);
+  const [lessonOutline, setLessonOutline] = useState(null);
+  const [editorLoaded, setEditorLoaded] = useState(false);
   const url = `http://localhost:8000/lessonplan/${params.lessonId}/`;
   let update = {}
+
+  useEffect(() => {
+    setEditorLoaded(true);
+  }, []);
 
   useEffect(() => {
     fetch(url)
@@ -33,9 +41,25 @@ export default function Unit({
     if(title !== null) update['title'] = title;
     if(objective !== null) update['objective'] = objective;
     if(standard !== null) update['standard'] = standard;
-  }, [title, objective, standard]);
+    if(body !== null) update['body'] = body;
+  }, [title, objective, standard, body]);
 
-  const updateButton = () => {
+  console.log(body)
+
+  const updateLessonButton = () => {
+    fetch(url, {      
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: 
+        JSON.stringify(update),
+    })
+    .then((response) => response.json())
+    .catch(error => console.log(error))
+  }
+
+  const updateLessonOutlineButton = () => {
     fetch(url, {      
       method: 'PATCH',
       headers: {
@@ -59,7 +83,7 @@ export default function Unit({
             <>
               <Surface>
                 <div className='space-y-4'>
-                  <form>
+                  <form className='space-y-3'>
                     <div className='flex justify-between'>
                       <Textarea
                         value={lesson.title}
@@ -68,7 +92,7 @@ export default function Unit({
                       />
                       <div className='flex flex-row gap-6'>
                         <button className='text-delete-light hover:text-delete' onClick={() => deleteLesson(params.lessonId)}>Delete</button>
-                        <button className='text-update-light hover:text-update' onClick={updateButton}>Update</button>
+                        <button className='text-update-light hover:text-update' onClick={updateLessonButton}>Update</button>
                       </div>
                     </div>
                     <div>
@@ -85,7 +109,7 @@ export default function Unit({
                         onChange={e => setStandard(e.target.value)}
                       />
                     </div>
-                    <div className='space-y-1'>
+                    {/* <div className='space-y-1'>
                       <h3 className='font-semibold'>Lesson Outline</h3>
                       { lesson.lesson_outline.length > 0 ? (
                         lesson.lesson_outline.map((outline) => (
@@ -100,6 +124,16 @@ export default function Unit({
                             <p>Add outline</p>
                           )
                         }
+                    </div> */}
+                    <div>
+                      <h3 className='font-semibold'>Lesson Outline</h3>
+                      <CKeditor
+                        name="description"
+                        onChange={(data) => {
+                          setBody(data);
+                        } }
+                        editorLoaded={editorLoaded} value={lesson.body}
+                      />
                     </div>
                     <div>
                       <h3 className='font-semibold'>Materials</h3>
