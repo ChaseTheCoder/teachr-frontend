07c3051c33dea6997c66b817e6adafc91c08a755
Chase Sheaff <chasefsheaff@gmail.com>
2024-09-11 21:40:43 -0500

    add plans layout

--- app/(loggedin)/plans/page.tsx
+++ app/(loggedin)/plans/page.tsx
@@ -1,82 +1,11 @@
-'use client'
-
 import React from 'react';
 import Surface from '../../../components/surface/Surface';
-import { Divider, Grid, IconButton, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
-import { useQuery } from '@tanstack/react-query';
-import { getDataNoUserId } from '../../../services/authenticatedApiCalls';
-import { MoreVert } from '@mui/icons-material';
 
-export default function Calendar() {
-  const { data: plansData, isFetching, isLoading, isError } = useQuery({
-    queryKey: ['plans'],
-    queryFn: () => getDataNoUserId('https://teachr-backend.onrender.com/subject/'),
-    staleTime: 1000 * 60 * 60,
-  })
-
-  if (isFetching || isLoading) {
-    return <span>Loading...</span>
-  }
-
-  if (isError) {
-    return <span>Error</span>
-  }
-
+export default function Plans() {
   return (
-    <Grid container spacing={1}>
-      <Grid item xs={3}>
-        <List sx={{bgcolor: '#ffffff', borderRadius: 4 }} dense disablePadding>
-          {plansData?.map((plan, index) => (
-            <>
-              {index !== 0 && <Divider />}
-              <ListItem key={plan.id}
-                secondaryAction={
-                  <IconButton edge="end" aria-label="comments">
-                    <MoreVert fontSize='small' />
-                  </IconButton>
-                }
-              >
-                <ListItemText
-                  primary={`${plan.grade}, ${plan.subject}`}
-                  primaryTypographyProps={{
-                    fontSize: 18,
-                    fontWeight: 'bold'
-                  }}
-                />
-              </ListItem>
-              {plan.units.map((unit) => (
-                <>
-                  <Divider variant="middle" component="li" />
-                  <ListItem
-                    key={unit.id}
-                    disablePadding
-                    secondaryAction={
-                      <IconButton edge="end" aria-label="comments">
-                        <MoreVert fontSize='small' />
-                      </IconButton>
-                    }
-                  >
-                    <ListItemButton>
-                      <ListItemText
-                        primary={unit.title}
-                        primaryTypographyProps={{
-                          fontSize: 14,
-                          fontWeight: 'bold'
-                        }}
-                      />
-                    </ListItemButton>
-                  </ListItem>
-                </>
-              ))}
-            </>
-          ))}
-        </List>
-      </Grid>
-      <Grid item xs={9}>
-        <Surface>
-          Lesson Plan
-        </Surface> 
-      </Grid>
-    </Grid>
-  )
-}
+    <Surface>
+      <h1>Welcome to the Page component!</h1>
+      <p>This is a basic page component.</p>
+    </Surface>
+  );
+};
---
+++ app/(loggedin)/plans/subject/[subjectId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Surface from '../../../../../components/surface/Surface';
+
+export default function subject() {
+  return (
+    <Surface>
+      <h1>Welcome to the Subject component!</h1>
+      <p>This is a basic page component.</p>
+    </Surface>
+  );
+};
