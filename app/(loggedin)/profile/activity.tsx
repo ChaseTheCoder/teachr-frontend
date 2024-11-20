ee706cae638a73b2af9934c7f131ae795ea672c5
Chase Sheaff <chasefsheaff@gmail.com>
2024-11-19 21:57:44 -0600

    add posts on profile page

--- app/(loggedin)/profile/activity.tsx
+++ app/(loggedin)/profile/activity.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Surface from '../../../components/surface/Surface';
 import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
-import { Box, Typography } from '@mui/material';
-import { AlignVerticalCenter } from '@mui/icons-material';
+import { Box, Skeleton, Typography } from '@mui/material';
+import { IPost } from '../../../types/types';
+import Post from '../../../components/post/post';
+import { useQuery } from '@tanstack/react-query';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { getData } from '../../../services/authenticatedApiCalls';
 
-const Activity: React.FC = () => {
+type Props = {
+  posts: IPost[] | null;
+}
+
+const NoActivity: React.FC = () => {
   return (
     <Surface>
       <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%', padding: '3rem' }} gap={2} >
@@ -12,6 +20,42 @@ const Activity: React.FC = () => {
         <Typography>No Posts or Comments Yet</Typography>
       </Box>
     </Surface>
+  )
+}
+
+const Activity: React.FC = () => {
+  const { user, error, isLoading: isLoadingUser } = useUser();
+  const auth0Id = user?.sub;
+
+  const { data: profileData, isFetching: isFetchingProfile, isLoading: isLoadingProfile, isError: isErrorProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: () => getData(`${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/profile_auth0/${auth0Id}`),
+    staleTime: 1000 * 60 * 60,
+    enabled: !!auth0Id,
+  })
+
+  const { data: posts, isFetching, isLoading, isError } = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => getData(`${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/posts/user/${profileData.id}`),
+    staleTime: 1000 * 60 * 60,
+    enabled: !!profileData,
+  })
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column' }} gap={.5}>
+      {isLoadingProfile || isLoading || isLoadingUser || isFetchingProfile || isFetching ? 
+        <Box sx={{ display: 'flex', flexDirection: 'column' }} gap={1}>
+          <Skeleton variant='rounded' height={80} />
+          <Skeleton variant='rounded' height={80} />
+          <Skeleton variant='rounded' height={80} />
+        </Box> :
+        posts ? 
+          posts.map((post) => (
+            <Post key={post.id} post={post} />
+          )) :
+          <NoActivity />
+      }
+    </Box>
   );
 };
 
---
+++ types/types.tsx
@@ -0,0 +1,7 @@
+export interface IPost {
+  id: string;
+  title: string;
+  body: string | null;
+  timestamp: string;
+  user: string;
+}
